a9af4a4b6aac35294f959b303e02a2abb8d2eed9
Keegan Fournier <jameskfour@gmail.com>
2025-11-27 23:41:28 -0800

    products page

--- javascript/image_scale.js
+++ javascript/image_scale.js
@@ -1,39 +1,59 @@
+//Allows an image to be clicked on and expanded for closer viewing by creating
+//a larger copy of the image, while adding a dimmed overlay to the page content
+//to bring focus to the expanded image. Both the image and overlay are
+//positioned fixed to maintain their coverage through any accidental scrolling of the page
 
+//variables
 let scaled = false;
+let expandedContainer = document.getElementById("expandedImageContainer");
+let overlay = document.getElementById("overlay");
 
-//create new container to avoid mucking up the background page?
 
+//applied directly to images, scales the image and toggles the dimmed overlay
 function scaleImage(img) {
-    let x = document.getElementById("expandedImageContainer");
-    let y = img.cloneNode(false);
-    x.appendChild(y);
+    //clone the image and add to the expanded container
+    let sourceImage = img.cloneNode(false);
+    expandedContainer.appendChild(sourceImage);
 
+    //if the image has not already been clicked on, makethe expanded container
+    //visible and style the image to be centered and cover 90% of the viewport height.
+    //otherwise remove any cloned images, hide the container and reset the component.
     if (!scaled) {
-        x.style.display = "block";
-        y.style.position = "fixed";
-        y.style.top = "50%";
-        y.style.left = "50%";
-        y.style.transform = "translate(-50%, -50%) scale(0.9)"; 
+        expandedContainer.style.display = "block";
+        sourceImage.style.position = "fixed";
+        sourceImage.style.width = "auto";
+        sourceImage.style.height = "90vh";
+        sourceImage.style.top = "50%";
+        sourceImage.style.left = "50%";
+        sourceImage.style.transform = "translate(-50%, -50%) scale(0.9)";
         scaled = true;
     } else {
-        removeAllChildNodes(x);
-        x.style.display = "none";
+        removeAllChildNodes(expandedContainer);
+        expandedContainer.style.display = "none";
         scaled = false;
     }
+    //toggle the dimmed overlay
     backgroundBlur();
 }
-
+//function to toggle the dimmed overlay
 function backgroundBlur() {
-let x = document.getElementById("overlay");
     if (scaled) {
-        x.style.display = "block";
+        overlay.style.display = "block";
     } else {
-        x.style.display = "none";
+        overlay.style.display = "none";
+    }
+}
+//function to remove child nodes from a specific node
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+//appplied to the overlay, resets both the enlarged container and dimmed overlay when clicked
+function reset() {
+    removeAllChildNodes(expandedContainer);
+    expandedContainer.style.display = "none";
+    overlay.style.display = "none";
+    scaled = false;
 }
