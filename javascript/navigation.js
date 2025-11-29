771e65d7e9a5cfdb00d316844f7ce6042e2312d8
Keegan Fournier <jameskfour@gmail.com>
2025-11-29 11:45:38 -0800

    navigation set up

---
+++ javascript/navigation.js
@@ -0,0 +1,19 @@
+
+let navBar = document.getElementById("topNav");
+let mediaQuery = window.matchMedia("(max-width: 640px");
+let opened = false;
+
+function toggleNav() {
+
+    if (mediaQuery.matches) {
+        if (opened) {
+            navBar.style.display = "none";
+            opened = false;
+        } else {
+            navBar.style.display = "block";
+            opened = true;
+        }
+    } else {
+        navBar.style.display = "block";
+    }
+}
