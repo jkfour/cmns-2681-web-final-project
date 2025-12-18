51d141d23b4f6cdb68c35742c6a50470f48fde47
Keegan Fournier <jameskfour@gmail.com>
2025-12-18 10:27:49 -0800

    fixes navigation not working onload from mobile

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -12,13 +12,18 @@ let firstSub = document.querySelectorAll("li.firstSubmenu");
 let secondSub = document.querySelectorAll("li.secondSubmenu");
 let full = document.querySelectorAll("li.fullNav")
 
+let mobileQuery = window.matchMedia("(max-width: 640px)")
+
 
 let opened = false;
 
 //Toggle the navigation bar when the mobile button is pressed
-function toggleNav() {
+function setLayout() {
     //Check if the current window with matches the media query for mobile widths
     if (mobileQuery.matches) {
+
+        navBar.style.display = "none";
+        console.log("match");
         //hide submenus
         firstSub.forEach(element => {
             element.style.display = "none";
@@ -27,15 +32,13 @@ function toggleNav() {
             element.style.display = "none";
         });
         full.forEach(element => {
-            element.style.display = "none";
+            element.style.display = "block";
         });
 
         //Then check if the mobile navigation bar is already open. 
         if (opened) {
             //if opened, hide the navigation
-            full.forEach(element => {
-                element.style.display = "block";
-            });
+            navBar.style.display= "none";
             opened = false;
         } else {
             //otherwise, show the navigation
@@ -51,13 +54,17 @@ function toggleNav() {
 //To handle resizing while the menu is open, add an event listener for resize
 window.addEventListener('resize', checkState);
 
+//Set the layout on load
+window.onload = checkState();
+
+
 //Check the state of the media query match
 function checkState() {
     //if the state does not match, as in, the window was resized from
     //mobile to desktop widths, ensure the navbar is displayed
     if (!mobileQuery.matches) {
         navBar.style.display = "grid";
-        toggleNav();
+        setLayout();
     }
     //otherwise, ensure the navbar is hidden by default
     else {
